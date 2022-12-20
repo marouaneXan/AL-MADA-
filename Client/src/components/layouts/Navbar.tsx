3c66c0fcb7e7227f00b5969cbde4f23aed4d83b8
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-20 17:34:42 +0100

    create a navbar component and setup router

---
+++ Client/src/components/layouts/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../../assets/shoes-logo.webp";
+// import SearchUser from "../SearchUser";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="container mx-auto p-6">
+      <div className="flex items-center justify-between">
+        <Link to={'/'}>
+          <img
+            src={logo}
+            alt="github logo"
+            className="rounded-full w-[50px] h-[50px] cursor-pointer"
+          />
+        </Link>
+        {/* {location.pathname !== "/" ? (
+          <Link
+            to="/"
+            className="block items-center py-2 mt-2 px-[18px] md:px-[38px] text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700"
+          >
+            Back To Home
+          </Link>
+        ) : (
+          <SearchUser/>
+        )} */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
