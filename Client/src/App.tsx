a4674440354289b73e36eb98740c111811472330
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-20 20:13:35 +0100

    setup @apollo/client

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,16 +1,24 @@
 import Products from "./components/Products/Products";
 import Navbar from "./components/layouts/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Products/>} />
-        </Routes>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Products />} />
+          </Routes>
+        </div>
+      </ApolloProvider>
     </Router>
   );
 }
