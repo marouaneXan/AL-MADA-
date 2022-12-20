37ed06571489946bb8915fcb834d0b5818393f63
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-20 16:42:15 +0100

    create a product type

---
+++ Server/schema/schema.js
@@ -0,0 +1,24 @@
+const Product=require('../models/Product')
+const {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLList
+}=require('graphql')
+
+//Product Type
+const ProductType=new GraphQLObjectType({
+    name:"Product",
+    fields:()=>({
+        id:{type:GraphQLID},
+        Brand_name:{type:GraphQLString},
+        IPR:{type:GraphQLString},
+        Status:{type:GraphQLString},
+        Number:{type:GraphQLString},
+        Office:{type:GraphQLString},
+        Owner:{type:GraphQLString},
+        Designation:{type:GraphQLString},
+        Nice_classification:{type:GraphQLString},
+    })
+})
