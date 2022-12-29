829c08fb69679b92cd79edfa88240c192289ef63
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-29 11:28:14 +0100

    add product in mutation

--- Client/src/Mutations/Product.ts
+++ Client/src/Mutations/Product.ts
@@ -1,9 +1,34 @@
-import {gql} from '@apollo/client'
-const DELETE_PRODUCT= gql`
-mutation deleteProduct($id:ID!){
-    deleteProduct(id:$id){
-        id
+import { gql } from "@apollo/client";
+const DELETE_PRODUCT = gql`
+  mutation deleteProduct($id: ID!) {
+    deleteProduct(id: $id) {
+      id
     }
-}
-`
-export {DELETE_PRODUCT}
+  }
+`;
+const ADD_PRODUCT = gql`
+  mutation addProduct(
+    $Brand_name: String!
+    $Designation: String!
+    $Number: String!
+    $Office: String!
+    $IPR: String!
+    $Owner: String!
+    $Nice_classification: String!
+    $Status: String!
+  ) {
+    addProduct(
+      Brand_name: $Brand_name
+      Designation: $Designation
+      Number: $Number
+      Office: $Office
+      IPR: $IPR
+      Owner: $Owner
+      Nice_classification: $Nice_classification
+      Status: $Status
+    ) {
+      id
+    }
+  }
+`;
+export { DELETE_PRODUCT, ADD_PRODUCT };
